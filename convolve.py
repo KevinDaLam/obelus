2ea2981d567f58840657166cb6b6d071659e2cfd
Justin Wang <Wang@fourwords.local>
2017-11-05 15:20:52 -0500

    initial test files

--- convolve.py
+++ convolve.py
@@ -1,5 +1,5 @@
 import filters
-import loader
+import matplotlib.pyplot as plt
 import numpy as np
 
 FILTERDIM = 9
@@ -14,26 +14,29 @@ class Filter():
     #TODO Implement dynamic stepping
     def convolve(self, imageData, imageSize):
         outputSize = imageSize - self.size
-        res = np.array((outputSize, outputSize), dtype = int))
+        res = np.empty((outputSize, outputSize), dtype = int)
         for x in range(outputSize):
             for y in range(outputSize):
                 sum = 0
                 for i in range(self.size):
                     for j in range(self.size):
-                        if imageData[x + i][y + j]:
+                        if imageData[x + i, y + j]:
                            sum += self.filter[i][j]
-                res[x][y] = sum         
+                res[x, y] = sum         
         return res
 
 class ConvolveLayer():
-    def __init__(self, fileName):
+    def __init__(self):
         self.filterList = []
-        for filter in filters.FILTERS:
-            self.filterList.append(Filter(FILTERDIM, filter)
+        for filter in filters.FILTERS9X9_2:
+            self.filterList.append(Filter(FILTERDIM, filter))
 
-    def convolve(image):
+    def convolve(self, image):
         output = np.empty(0, dtype=int)
         for filter in self.filterList:
-            out = filter.convolve(image, IMAGEDIM)
-            np.append(self.output, out.flatten())
+            out = filter.convolve(image, IMAGEDIM)*75
+            plt.imshow(out, cmap="hot", interpolation="nearest")
+            plt.show()
+            output = np.append(output, out.flatten())
+            print out.flatten(), np.size(out)
         return output
--- loader.py
+++ loader.py
@@ -8,16 +8,35 @@ def loadData(fileName):
     imageDict = {} 
     with open(fileName) as file:
         imageName = None
-        imageData = np.empty((DIMENSION,DIMENSION), dtype=bool)
+        imageData = []
         counter = 0
         for line in file:
             if counter == 0:
                 imageName = line.strip()
             else:
-                imageData[counter - 1] = np.array([x == BLACK for x in line.strip().split(DELIM)])
+                imageData.append([x == BLACK for x in line.strip().split(DELIM)])
                 if counter == DIMENSION:
-                    imageDict[imageName] = imageData
-                    imageData = np.empty((DIMENSION,DIMENSION), dtype=bool)
+                    imageDict[imageName] = np.asarray(imageData)
+                    imageData = []
                     counter = -1
             counter += 1
-    return imageDict    
+    return imageDict
+
+def loadBinary(fileName):
+    imageList = []
+    with open(fileName) as file:
+        imageName = None
+        imageData = []
+        counter = 0
+        for line in file:
+            if counter == 0:
+                imageName = line.strip()
+            else:
+                for x in line.strip().split(DELIM):
+                    imageData.append(int(x))
+                if counter == DIMENSION:
+                    imageList.append(imageData)
+                    imageData = []
+                    counter = -1
+            counter += 1
+    return np.asarray(imageList)
